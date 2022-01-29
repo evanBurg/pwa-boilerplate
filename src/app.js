f9dc09bece6a397db698abe76eaf850c67fed63d
Evan Burgess <evan@evenburgers.com>
2022-01-28 21:01:48 -0500

    Share target setup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-document.querySelector('#share').addEventListener('click', () => {
-  if (navigator.share) {
-    navigator.share({
-      title: 'This is subject',
-      text: 'This is the body',
-      url: 'https://dev.localhost/',
-    })
-      .then(() => console.log('Successful share'))
-      .catch((error) => console.log('Error sharing', error));
+const shareBtn = document.querySelector('#share');
+if (shareBtn) {
+  shareBtn.addEventListener('click', () => {
+    if (navigator.share) {
+      navigator.share({
+        title: 'This is subject',
+        text: 'This is the body',
+        url: 'https://dev.localhost/',
+      })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    }
+  });
+}
+
+if (window.location.href.includes('sharing')) {
+  const parsedURL = new URL(window.location);
+  const title = parsedURL.searchParams.get('title');
+  const text = parsedURL.searchParams.get('text');
+  const url = parsedURL.searchParams.get('url');
+  const resultParagraph = document.querySelector('#share-result');
+  if (resultParagraph) {
+    resultParagraph.innerHTML = `
+      ${title || ''}<br/>
+      ${text || ''}<br/>
+      ${url || ''}<br/>
+    `;
   }
-})
+}
